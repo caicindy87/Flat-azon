4f2468823ada76bb88d5ebd1c787ae3d33dbbddf
caicindy87 <caicindy87@gmail.com>
2020-04-14 09:51:20 -0700

    Finished new item form and routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,25 @@
 class ItemsController < ApplicationController
+    def new
+        @item = Item.new
+    end
+
+    def create
+        @item = Item.new(item_params)
+
+        if @item.valid?
+            @item.save
+            redirect_to @item
+        else
+            render :new
+        end
+    end
+
     def show
         @item = Item.find(params[:id])
+    end
+
+    private
+    def item_params
+        params.require(:item).permit(:name, :quantity, :price,:description, :picture, :category_id)
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            flash[:success] = 'You loged in'
+            flash[:success] = 'You logged in'
             redirect_to @user
         else
             flash[:error] = 'Invalid credentials'
