15ec664763ec16b0d8b5db5e27e08fef467aad1c
lalogaribo <garibo.eduardo90@gmail.com>
2020-04-15 07:44:40 -0700

    Updated navbar with username populated from database, added gravatar helper to load profile images.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,60 +1,64 @@
 class ItemsController < ApplicationController
-    before_action :find_item, only: [:show, :edit, :update, :destroy]
-    before_action :authorized, except: [:show]
-    before_action :current_user, except: [:show] 
-    before_action :check_item_user, only: [:edit, :update]
+  before_action :find_item, only: [:show, :edit, :update, :destroy, :same_user_item]
+  before_action :authorized, except: [:show]
+  before_action :current_user, except: [:show]
+  before_action :same_user_item, only: [:edit, :update]
 
-    def new
-        @item = Item.new
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.valid?
+      @item.save
+      redirect_to @item
+    else
+      render :new
     end
+  end
 
-    def create
-        @item = Item.new(item_params)
+  def show
+  end
 
-        if @item.valid?
-            @item.save
-            redirect_to @item
-        else
-            render :new
-        end
+  def edit
+  end
+
+  def update
+    if @item.valid?
+      @item.update(item_params)
+      @item.save
+      redirect_to @item
+    else
+      render :edit
     end
+  end
 
-    def show
+  def destroy
+    @item.delete
+    redirect_to categories_path
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :quantity, :price, :description, :picture, :category_id, :user_id)
+  end
+
+  def check_item_user
+    @user_id = Item.find(params[:id]).user_id
+    if !(@user_id == current_user.id)
+      flash[:error] = "Not authorized to edit this item"
+      redirect_to item_path(params[:id])
     end
+  end
 
-    def edit
-    end
-
-    def update
-            if @item.valid?
-                @item.update(item_params)
-                @item.save
-                redirect_to @item
-            else
-              render :edit
-            end
-    end
-
-    def destroy
-        @item.delete
-        redirect_to categories_path
-    end
-
-    private
-
-    def find_item
-        @item = Item.find(params[:id])
-    end
-
-    def item_params
-        params.require(:item).permit(:name, :quantity, :price,:description, :picture, :category_id, :user_id)
-    end
-
-    def check_item_user
-        @user_id = Item.find(params[:id]).user_id
-        if !(@user_id == current_user.id)
-          flash[:error] = "Not authorized to edit this item"
-          redirect_to item_path(params[:id])
-        end
-      end
+  def same_user_item
+    not_authorized(@item, 'Not authorized to edit this item')
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 100})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm"
+    image_tag(gravatar_url, alt: user.username, class: 'avatar')
+  end
 end
