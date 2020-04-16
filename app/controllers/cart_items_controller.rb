212460f07333fcbd9c7ad0fac39f4b1a96076c18
caicindy87 <caicindy87@gmail.com>
2020-04-16 11:07:14 -0700

    Working on adding item to cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,9 +2,16 @@ class CartItemsController < ApplicationController
   before_action :set_cart, only: [:create, :destroy]
 
   def create
-    @cart_item = CartItem.new(cart_id: @cart.id)
+    # byebug
+    @cart.add_item(cart_item_params)
   end
 
   def destroy
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity)
   end
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,15 @@ class Cart < ApplicationRecord
   belongs_to :user_id
   has_many :cart_items
 
+  def add_item(cart_item_params)
+    current_item = CartItem.find_by(item_id: cart_item_params[:cart_item][:item_id])
+    if current_item
+      current_item.quantity += 1
+      current_item.save
+    else
+      new_item = CartItem.create(cart_id: self.id, item_id: cart_item_params[:cart_item][:item_id], quantity: 1)
+    end
+    new_item
+  end
+
 end
