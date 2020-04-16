2d089e3b1a370ebb0e32ce60a4b9226c8c97c60b
SaberChulainn <Tbandachit@gmail.com>
2020-04-16 15:04:51 -0700

    added styling to item page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username: "sushirocks", password: "hi", first_name: "Lisa", last_name: "Li", email: "lisa@example.com", address: "123 North Street")
-
 Category.create(name: "Furniture")
 Category.create(name: "Electronics")
 Category.create(name: "Clothing")
